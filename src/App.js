262cc0f836ad2cc9df18eab307728c0c951cf787
ferutizi <fer.utizi@gmail.com>
2022-12-29 23:50:49 -0300

    fetchData

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    fetchData();  
+    console.log(dogs);
+  }, [])
+  
+
+  const fetchData = async () => {
+    const res = await fetch('https://dog.ceo/api/breed/hound/images');
+    const data = await res.json();
+    setDogs(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Dogs</h1>
+    </>
   );
 }
 
