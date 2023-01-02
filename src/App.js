af0879feaa843742138ae5a317e0c01dd72c01cb
ferutizi <fer.utizi@gmail.com>
2023-01-02 11:52:58 -0300

    form and search input

--- src/App.js
+++ src/App.js
@@ -2,28 +2,54 @@ import './App.css';
 import { useEffect, useState } from "react";
 
 function App() {
-  const [dogs, setDogs] = useState([]);
+  const [formulario, setFormulario] = useState({name: ''})
+  const [breed, setBreed] = useState([]);
 
   useEffect(() => {
-    fetchData();  
-  }, [])
-  
+      fetchData();  
+  }, []);
 
   const fetchData = async () => {
     const res = await fetch('https://dog.ceo/api/breed/hound/images');
     const data = await res.json();
-    setDogs(data.message);
+    setBreed(data.message);
   }
 
-  fetchData();
+  const handleChange = (e) => {
+    setFormulario({
+      ...formulario,
+      [e.target.name]: e.target.value
+    });
+    console.log(formulario);
+    
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setBreed([
+      ...breed,
+      formulario.name
+    ]);
+    setFormulario({name: ''});
+  }
 
   return (
     <div className="App">
       <h1>Dogs</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          name='name'
+          type='text'
+          value={formulario.name}
+          onChange={handleChange}
+          autoFocus
+        />
+        <button>Search</button>
+      </form>
       <div className="dogs__container">
-        {dogs.length > 0 ?
-          dogs.slice(0, 15).map(item => 
-              <img className="dogs__img" src={item}></img>
+        {breed.length > 0 ?
+          breed.slice(0, 15).map(item => 
+              <img key={item} className="dogs__img" src={item}></img>
             )
           : <p>loading...</p>
         }
