91ff9a4bbc91ffe087e14d850a6cdcf188d3b30a
ferutizi <fer.utizi@gmail.com>
2024-02-13 16:56:26 -0300

    fixed app crash when no results were found

--- src/components/Dogs.jsx
+++ src/components/Dogs.jsx
@@ -1,6 +1,6 @@
 import './Dogs.css';
 
-const Dogs = ({ breeds, fav, setFav }) => {
+const Dogs = ({ breeds, fav, setFav, breed }) => {
 
     const addFav = (dog) => {
         const newFav = fav.find(item => item == dog)
@@ -9,22 +9,22 @@ const Dogs = ({ breeds, fav, setFav }) => {
         } else {
           setFav(fav.filter(item => item !== dog))
         }
-      console.log(fav)
     }
 
-    return(
-        <div className="dogs__container">
-        {breeds.length > 0 ?
-          breeds.slice(0, 15).map(item => 
-            <div key={item} className='img__container'>
-              <img src={item} className="dogs__img"></img>
-              <span onClick={() => addFav(item)} className="dog__heart">{fav.find(e => e == item) ? '❤️' : '🤍'}</span>
-            </div>
-          )
-          : <p>Loading...</p>
-        }
-        </div>
-    );
+    return (
+      <div className="dogs__container">
+          {Array.isArray(breeds) && breeds.length > 0 ? (
+              breeds.slice(0, 15).map(item => 
+                  <div key={item} className='img__container'>
+                      <img src={item} className="dogs__img" alt={breed} title={breed}></img>
+                      <span onClick={() => addFav(item)} className="dog__heart">{fav.find(e => e === item) ? '❤️' : '🤍'}</span>
+                  </div>
+              )
+          ) : (
+              <p>No dogs found</p>
+          )}
+      </div>
+  );
 }
 
 export default Dogs;
